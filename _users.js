d3348f7c7a1d8ea4314a479c2c14946d7b4e285a
AdamBenes98 <adamekbenes@gmail.com>
2025-10-18 14:31:56 +0200

    Delete _users.js

--- _users.js
+++
@@ -1,5 +0,0 @@
-/* ONLY YOU edit this file.  Generate hashes with https://bcrypt-generator.com/ (rounds 10) */
-const USER_HASHES = {
-  "alice":  "$2b$10$Hjd3XqQ8o8m0WQ8hCjyJdO39BV1GpZpwR2hKc4G3Fzb8LHHXo4Ogq",  // password: alice123
-  "bob":    "$2b$10$4qO5uuh8Y4FzRkR4Yc1dDeH5Jc4gF3H5b8LHHXo4Ogq"   // password: bob456
-};

ffb793552d3ffa2c1da2497c24755c4c19733d6b
AdamBenes98 <adamekbenes@gmail.com>
2025-10-19 13:23:20 +0200

    Update vault.js

--- api/vault.js
+++ api/vault.js
@@ -1,1 +1,29 @@
+import { readFile, writeFile } from 'fs/promises'
+import { join } from 'path'
 
+const DB = join(process.cwd(),'data','db.json')
+
+async function getDb(){
+  try{
+    return JSON.parse(await readFile(DB,'utf8'))
+  }catch{ return {news:[], polls:{}, vault:[], archive:[] } }
+}
+
+async function saveDb(obj){
+  await writeFile(DB, JSON.stringify(obj,null,2))
+}
+
+export default async function handler(req, res){
+  const db = await getDb()
+  if(req.method==='GET'){
+    res.status(200).json(db.vault||[])
+  }else if(req.method==='POST'){
+    const memo = req.body          // {text, time, file}
+    if(!db.vault) db.vault = []
+    db.vault.unshift(memo)
+    await saveDb(db)
+    res.status(201).json({ok:true})
+  }else{
+    res.status(405).end()
+  }
+}

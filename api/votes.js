19ea2883646e63ad5ebf933c02d939a7633de1a9
AdamBenes98 <adamekbenes@gmail.com>
2025-10-19 13:21:40 +0200

    Update votes.js

--- api/votes.js
+++ api/votes.js
@@ -1,1 +1,20 @@
+import { readFile, writeFile } from 'fs/promises'
+import { join } from 'path'
+const DB = join(process.cwd(),'data','db.json')
 
+async function getDb(){ …same… }
+async function saveDb(obj){ …same… }
+
+export default async function handler(req, res){
+  const db = await getDb()
+  if(req.method==='GET'){
+    res.status(200).json(db.polls||{})   // {title,end,votes:{}}
+  }else if(req.method==='POST'){
+    const {user, vote} = req.body        // vote = true/false
+    if(!db.polls) db.polls = { votes:{} }
+    if(db.polls.votes[user]) return res.status(409).json({error:'already voted'})
+    db.polls.votes[user] = vote
+    await saveDb(db)
+    res.status(200).json({ok:true})
+  }
+}

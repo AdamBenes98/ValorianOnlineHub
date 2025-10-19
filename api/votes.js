eb4d54a02369ae2bebff0b0ffd60c720523a4cd1
AdamBenes98 <adamekbenes@gmail.com>
2025-10-19 14:16:58 +0200

    Update votes.js

--- api/votes.js
+++ api/votes.js
@@ -1,20 +1,21 @@
-import { readFile, writeFile } from 'fs/promises'
-import { join } from 'path'
-const DB = join(process.cwd(),'data','db.json')
+import { kv } from '@vercel/kv'
 
-async function getDb(){ …same… }
-async function saveDb(obj){ …same… }
-
-export default async function handler(req, res){
-  const db = await getDb()
-  if(req.method==='GET'){
-    res.status(200).json(db.polls||{})   // {title,end,votes:{}}
-  }else if(req.method==='POST'){
-    const {user, vote} = req.body        // vote = true/false
-    if(!db.polls) db.polls = { votes:{} }
-    if(db.polls.votes[user]) return res.status(409).json({error:'already voted'})
-    db.polls.votes[user] = vote
-    await saveDb(db)
-    res.status(200).json({ok:true})
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const polls = (await kv.get('polls')) || {}
+    return res.status(200).json(polls)
   }
+
+  if (req.method === 'POST') {
+    const { user, vote } = req.body        // vote = true/false
+    const polls = (await kv.get('polls')) || { votes: {} }
+
+    if (polls.votes[user]) return res.status(409).json({ error: 'already voted' })
+
+    polls.votes[user] = vote
+    await kv.set('polls', polls)
+    return res.status(200).json({ ok: true })
+  }
+
+  res.status(405).end()
 }
